97f7408af7ea85cc56ea05ceb6e182ab3078462f
Mike <mclancy96@gmail.com>
2025-05-20 09:36:21 -0400

    Add type testing for 3

--- test/section3_bonus_validateInput.test.ts
+++ test/section3_bonus_validateInput.test.ts
@@ -3,12 +3,13 @@ import * as ts from "typescript";
 import { readFileSync } from "fs";
 import { join } from "path";
 import vm from "vm";
+import * as type_annotation from "chai_typescript_type_annotation_tests";
 
 describe("Lab 3 — Section 3 (Bonus): validateInput", () => {
   let context: any = {};
+  const filePath = join(__dirname, "../src/section3_bonus_validateInput.ts");
 
   before(() => {
-    const filePath = join(__dirname, "../src/section3_bonus_validateInput.ts");
     const tsCode = readFileSync(filePath, "utf8");
     const jsCode = ts.transpile(tsCode);
     vm.createContext(context);
@@ -20,7 +21,7 @@ describe("Lab 3 — Section 3 (Bonus): validateInput", () => {
   });
 
   it("should return false for empty string", () => {
-    expect(context.validateInput(" ")).to.equal(false);
+    expect(context.validateInput("")).to.equal(false);
   });
 
   it("should return true for number > 0", () => {
@@ -38,4 +39,16 @@ describe("Lab 3 — Section 3 (Bonus): validateInput", () => {
   it("should return false if object has valid: false", () => {
     expect(context.validateInput({ valid: false })).to.equal(false);
   });
+
+  type_annotation.expectFunctionReturnTypeAnnotation(
+    filePath,
+    "validateInput",
+    "boolean"
+  );
+
+  type_annotation.matchFunctionParameterTypeAnnotation(
+    filePath,
+    "validateInput",
+    ["string | number | object"]
+  );
 });
